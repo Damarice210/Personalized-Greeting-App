4d45a9507669c3914882f6d9b9189b47235dff74
aluochkure@gmail.com <aluochkure@gmail.com>
2024-11-09 16:44:14 +0300

    greetings app

---
+++ greetings.py
@@ -0,0 +1,10 @@
+# Personalized Greeting App 👋
+
+# Ask for the user's name
+name = input("What's your name? ")
+
+# Ask for the user's favorite color
+favorite_color = input("What's your favorite color? ")
+
+# Print the personalized greeting
+print(f"Hello, {name}! Your favorite color, {favorite_color}, is awesome!")
